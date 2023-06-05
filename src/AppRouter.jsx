8d85bf91037375a75a992cb1c856f10f66dc1345
jlopez-2018422 <jlopez-2018422@kinal.edu.gt>
2023-06-05 11:16:02 -0600

    Agregar LoginAdmin, api, component, helper, actualizar AppRouter

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,20 +1,32 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LoginUser } from "./loginUser/components/LoginUser";
 import { NavBar } from "./Navbar";
 import { Footer } from "./Footer";
 import { HomePage } from "./HomePage";
-
-
+import { isAdminAuthenticated } from "./LoginAdminUser/helpers/loginAdminHelper";
+import { LoginAdminUser } from "./LoginAdminUser/components/LoginAdminUser";
 
 export const AppRouter = () => {
-  const acepta = true;
   return (
-    <>
-      <NavBar></NavBar>
+    <BrowserRouter>
+      <NavBar />
 
-      <HomePage></HomePage>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/loginUser" element={<LoginUser />} />
+        <Route path="/loginAdmin" element={
+            isAdminAuthenticated() ? (
+              <LoginAdminUser></LoginAdminUser>
+            ) : (
+              <Navigate to="/HomePage"></Navigate>
+            )
+          } />
 
+        {/* Agrega más rutas según tus necesidades */}
+      </Routes>
 
-      <Footer></Footer>
-    </>
+      <Footer />
+    </BrowserRouter>
   );
 };
---
+++ src/paginaPrincipal/components/PaginaPrincipal.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export const PaginaPrincipal = () => {
+    return (
+
+
+        <>
+            <div className="containerc">
+                <div className="box">
+                    <div className="content">
+                        <h3>Cuadro 1</h3>
+                        <p>Contenido del cuadro 1</p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="content">
+                        <h3>Cuadro 2</h3>
+                        <p>Contenido del cuadro 2</p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="content">
+                        <h3>Cuadro 3</h3>
+                        <p>Contenido del cuadro 3</p>
+                    </div>
+                </div>
+                <div className="box">
+                    <div className="content">
+                        <h3>Cuadro 4</h3>
+                        <p>Contenido del cuadro 4</p>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
