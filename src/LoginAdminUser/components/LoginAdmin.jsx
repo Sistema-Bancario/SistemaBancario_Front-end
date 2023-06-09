7f4f7f79f0308a9a4647b4d574764fb154ce8c34
Jefrey Lopez <jlopez-2018422@kinal.edu.gt>
2023-06-09 16:09:58 -0600

    Modificando Logins, navbars

--- src/LoginAdminUser/components/LoginAdmin.jsx
+++ src/LoginAdminUser/components/LoginAdmin.jsx
@@ -12,6 +12,8 @@ import {
 } from "mdb-react-ui-kit";
 import { Button } from "react-bootstrap";
 import { apiLoginAdmin } from '../api/apiLoginAdmin';
+import { Link } from 'react-router-dom';
+
 
 export const LoginAdmin = () => {
   const [correo, setCorreo] = useState("");
@@ -39,19 +41,19 @@ export const LoginAdmin = () => {
       <MDBRow>
         <MDBCol sm="6">
           <div className="d-flex flex-row ps-5 pt-5">
-            <span id="title" style={{ paddingLeft:"80px" }} className="h1 fw-bold mb-0">
-              Bank
+            <span id="title" style={{ paddingLeft: "80px" }} className="h1 fw-bold mb-0">
+            LOG IN ADMIN
             </span>
           </div>
+          <br></br>
+            <br></br>
 
           <div className="d-flex flex-column justify-content-center h-custom-2 w-75 pt-4">
-            <h3
-              className="fw-normal mt-5 mb-3 ps-5 pb-3"
-              style={{ letterSpacing: "0px", paddingLeft:"50px" }}
-              id="iniciar-sesion"
-            >
-              LOG IN ADMIN
-            </h3>
+            <Link to="/"><Button className='regresarAdmin'>Regresar</Button></Link>
+            <br></br>
+            <br></br>
+            <br></br>
+            
 
             <form onSubmit={handleLoginSubmit}>
               <MDBInput
@@ -89,18 +91,18 @@ export const LoginAdmin = () => {
                 Iniciar Sesion
               </Button>
 
-            
+
             </form>
           </div>
         </MDBCol>
 
         <MDBCol sm="6" className="d-none d-sm-block px-0">
           <img
-            src="BankLogo.png"
+            src="https://www.baccredomatic.com/sites/default/files/styles/rectangle_650x750/public/2021-04/TorreBAC7_0.jpg?itok=XJsaqK7I"
             id="imagen-login"
             alt="Login image"
             className="w-100"
-            style={{ objectFit: "cover", textAlign:"center" }}
+            style={{ objectFit: "cover", textAlign: "center" }}
           />
         </MDBCol>
       </MDBRow>
--- src/NavbarUser.jsx
+++ src/NavbarUser.jsx
@@ -1,46 +1,53 @@
-  import React from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom';
 
-  const NavbarUser = () => {
-    return (
-      <>
-        <div class="contaaainer">
-          <div id="sidebar">
-            <div class="sidebar-header">
-              <h1 class="sidebar-title">BAC</h1>
-              <div class="circle-image">
-                <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="fotoperfil" />
-              </div>
+const NavbarUser = () => {
+  return (
+    <>
+      <div class="contaaainer">
+        <div id="sidebar">
+          <div class="sidebar-header">
+            <h1 class="sidebar-title">BAC</h1>
+            <div class="circle-image">
+              <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="fotoperfil" />
             </div>
-
-            <nav class="sidebar-navigation">
-              <ul>
-                <li class="active">
-                  <i class="fa fa-share-alt"></i>
-                  <span class="tooltip">Agregar Usuarios</span>
-                </li>
-                <li>
-                  <i class="fa fa-hdd-o"></i>
-                  <span class="tooltip">Crear Cuenta</span>
-                </li>
-                <li>
-                  <i class="fa fa-newspaper-o"></i>
-                  <span class="tooltip">Contacts</span>
-                </li>
-                <li>
-                  <i class="fa fa-print"></i>
-                  <span class="tooltip">Fax</span>
-                </li>
-                <li>
-                  <i class="fa fa-sliders"></i>
-                  <span class="tooltip">Settings</span>
-                </li>
-              </ul>
-            </nav>
           </div>
+
+          <nav class="sidebar-navigation">
+            <ul>
+              <li class="active">
+                <Link className='botoagregarUsuarios' to="/createUser">
+                  <i class="fa fa-user"></i>
+                  <span class="tooltip">Ver cuentas</span>
+                </Link>
+              </li>
+              <li>
+                <Link className='botoagregarUsuarios' to="/createUser">
+                  <i class="fa fa-exchange"></i>
+                  <span class="tooltip">Transferencias</span>
+                </Link>
+              </li>
+              <li>
+                <Link className='botoagregarUsuarios' to="/createUser">
+                  <i class="fa fa-history"></i>
+                  <span class="tooltip">Historial</span>
+                </Link>
+              </li>
+              <li>
+                <Link className='botoagregarUsuarios' to="/createUser">
+                  <i class="fa fa-star"></i>
+                  <span class="tooltip">Favoritos</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+
         </div>
+      </div>
 
-      </>
-    );
-  };
+    </>
+  );
+};
 
-  export default NavbarUser
+export default NavbarUser
